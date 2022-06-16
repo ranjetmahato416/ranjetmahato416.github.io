806d43e548c8c84268be2560c76f86ff040255b0
ranjit <ranjit@gmail.com>
2022-06-16 08:26:36 +0545

    Existing Files

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "UI/UX Designer","React Developer", "Django Developer"],
+        strings: [ "Front-End Developer","React Developer", "Django Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "UI/UX Designer","React Developer", "Django Developer"],
+        strings: [ "Front-End Developer","React Developer", "Django Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
