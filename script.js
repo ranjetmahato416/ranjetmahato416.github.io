0b24ed2e0e71a8f7720d3165bbd2231c55ba0f14
ranjetaimhighnepal <ranjetmahato416@gmail.com>
2023-07-20 13:47:35 +0545

    portfolio update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Front-End Developer","React Developer", "Django Developer"],
+        strings: ["Angular.TS Developer","Front-End Developer", "JavaScript Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Front-End Developer","React Developer", "Django Developer"],
+        strings: ["Angular.TS Developer","Front-End Developer", "JavaScript Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
